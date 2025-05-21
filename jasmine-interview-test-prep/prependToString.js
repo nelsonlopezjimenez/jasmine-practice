19843e31d710c0273ec3808efbdd8b8210b28d83
Local Epsilon User <localepsilon@cis0678>
2025-05-21 05:46:21 -0700

    removeFromString slice method explained

--- jasmine-interview-test-prep/prependToString.js
+++ jasmine-interview-test-prep/prependToString.js
@@ -1,2 +1,18 @@
-function prependToString() {
+function prependToString(str, start, end) {
+
 }
+
+// prependToString
+
+// Write a function called **prependToString,** which accepts two strings.
+
+// The function should return a new string with the second string prepended to the first string.
+
+// Examples:
+
+// ```js
+console.log(prependToString('awesome', 'very')); // 'veryawesome'
+console.log(prependToString('world', 'hello ')); // 'hello world'
+console.log(prependToString('nothing', '')); // 'nothing'
+// ```
+
--- jasmine-interview-test-prep/removeFromString.js
+++ jasmine-interview-test-prep/removeFromString.js
@@ -1,3 +1,37 @@
-const removeFromString = () => {
-
+const removeFromString = (str, start, end) => {
+    return str.slice(start, end);
 }
+
+// removeFromString
+
+// Write a function called **removeFromString,** which accepts a string, a starting index (number) and a number of characters to remove.
+
+// The function should return a new string with the characters removed.
+
+// Examples:
+
+// ```js
+console.log(removeFromString('Elie', 2, 2)); // 'El'
+console.log(removeFromString('Elie', 0, 1)); // 'lie'
+console.log(removeFromString('Rithm School', 0, 6)); // 'School'
+console.log(removeFromString('Rithm School', 2, 4)); // 'RiSchool'
+console.log(removeFromString('Rithm School', 6, 400)); // 'Rithm '
+
+/**Description
+The slice() method extracts a part of a string.
+
+The slice() method returns the extracted part in a new string.
+
+The slice() method does not change the original string.
+
+The start and end parameters specifies the part of the string to extract.
+
+The first position is 0, the second is 1, ...
+
+A negative number selects from the end of the string. 
+*/
+
+/**Return Value
+
+A string	The extracted part of the string. 
+*/
